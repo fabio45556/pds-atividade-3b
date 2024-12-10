eb9c8e680e47b56a6fdb5cc3ab6f6d9964162a55
Fábio de Paula Santos <20211064010019@ifrn.local>
2024-12-10 15:04:53 -0300

    Projeto Pronto!

---
+++ Atividade/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        // Criando uma instância de Pessoa para cada estuantes
+        Pessoa pessoa = new Pessoa("Daniel", 25);
+        Pessoa pessoa2 = new Pessoa("Fábio de Paula Santos", 19);
+        
+
+        // Exibindo as informações dos estudantes
+        pessoa.exibirInformacoes();
+        pessoa2.exibirInformacoes();
+    }
+}
